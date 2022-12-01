76dd291cc5f721e51c32efb4d9daae71c458d1cd
Nina Ceh <71320289+nina-balerina@users.noreply.github.com>
2022-12-01 10:34:50 +0100

    Day 01

---
+++ day01.py
@@ -0,0 +1,19 @@
+f = open('day01.txt', 'r')
+txt = f.readlines()
+data = []
+counter = 0
+suma = []
+suma.append(0)
+for line in txt:
+    if line == '\n':
+        counter +=1
+        suma.append(0)
+    else:
+        data.append(int(line))
+        suma[counter] += int(line)
+
+print('part 1: %s' % max(suma)) 
+
+suma.sort()
+print('part 2: %s' % sum(suma[counter-2: counter+1])) 
+print()
