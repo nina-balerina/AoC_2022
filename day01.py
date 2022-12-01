6185fda36727fa3f10488685f09442354976f3c8
Nina Ceh <71320289+nina-balerina@users.noreply.github.com>
2022-12-01 10:58:56 +0100

    Update day01.py

--- day01.py
+++ day01.py
@@ -1,6 +1,5 @@
 f = open('day01.txt', 'r')
 txt = f.readlines()
-data = []
 counter = 0
 suma = []
 suma.append(0)
@@ -9,10 +8,9 @@ for line in txt:
         counter +=1
         suma.append(0)
     else:
-        data.append(int(line))
         suma[counter] += int(line)
 
-print('part 1: %s' % max(suma)) 
+print('part 1:', max(suma)) 
 
 suma.sort()
-print('part 2: %s' % sum(suma[counter-2: counter+1])) 
+print('part 2:', sum(suma[counter-2: counter+1])) 
